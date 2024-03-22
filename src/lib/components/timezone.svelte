0e04744c2445a8efe8085c22223561e0d8ea5a65
Yong Lin <yonglin@me.com>
2024-03-22 13:41:13 +0800

    Somewhat working

---
+++ src/lib/components/timezone.svelte
@@ -0,0 +1,159 @@
+<script>
+	import * as d3 from 'd3';
+	import { onMount } from 'svelte';
+	import { readable } from "svelte/store";
+
+
+	/**
+	 * zone_name,country_code,abbreviation,time_start,gmt_offset,dst
+	 * @typedef {Object} Timezone
+	 * @property {string} zone_name
+	 * @property {string} country_code
+	 * @property {string} abbreviation
+	 * @property {EpochTimeStamp} time_start 
+	 * @property {number} gmt_offset in seconds
+	 * @property {boolean} dst Whether daylight savings is in effect
+	 */
+
+    /** @type {import("svelte/store").Readable<Record<string, string>>} */
+    export let timezoneNames;
+
+    /** @type {import("svelte/store").Readable<Record<string, Timezone[]>>} */
+    export let timezones;
+
+
+    export let value = "";
+    $: value = $timezoneNames[dropdownValue];
+
+    let searchString = "";
+
+    export let dropdownValue = value;
+
+    $: timezoneSearchResults = Object.keys($timezoneNames).filter(name => searchString.trim() == "" ? true : name.toLowerCase().includes(searchString.trim().toLowerCase()));
+
+    /** 
+     * @param {KeyboardEvent} event
+     */
+    function onSearchStringChange(event) {
+        if(!timezoneSearchResults.includes(dropdownValue)) {
+            dropdownValue = timezoneSearchResults[0];
+        }
+    }
+
+    /** 
+     * An array of ticks for each hour across 3 days, centred on the current time
+     * @type {number[]}
+     */
+    export let hourTicks = Array.from({ length: 72 }, (_, i) => {
+        let now = Date.now();
+        let hour = 1000 * 60 * 60;
+        let offset = (i - 36) * hour;
+        return now + offset;
+    });
+
+    let x = d3.scaleTime()
+
+    $: x.domain(d3.extent(hourTicks));
+    
+    let y = d3.scaleLinear()
+        .domain([0,1])
+
+    /** @type {SVGSVGElement} */
+    let svg;
+
+    function onresize() {
+        if(svg) {
+            x.range([0, svg.clientWidth]);
+            y.range([10, svg.clientHeight-10]);
+        }
+        x = x;
+        y = y;
+    }
+
+    onMount(() => {
+        window.addEventListener("resize", onresize);
+        onresize();
+
+        return () => {
+            window.removeEventListener("resize", onresize);
+        }
+    });
+
+    $: timezoneOffset = $timezones[dropdownValue].find(tz => tz.time_start <= Date.now())?.gmt_offset ?? 0;
+
+    $: dateInSelectedTimezone = 0;
+</script>
+
+<div class="timezone">
+    <div>
+        <input type="search" class="search" bind:value={searchString} placeholder="Search" on:keydown={onSearchStringChange} />
+        <select class="timezone" bind:value={dropdownValue}>
+            {#each timezoneSearchResults as timezoneName}
+                <option value={timezoneName}>{timezoneName}</option>
+            {/each}
+        </select>
+    </div>
+    <svg bind:this={svg} height="3em">
+        <g class="ticks">
+            {#each hourTicks as tick}
+                <line x1={x(tick)} x2={x(tick)} y1={y(0)} y2={y(1)} />
+            {/each}
+        </g>
+        <g class="now">
+            <line x1={x(Date.now())} x2={x(Date.now())} y1={y(0)} y2={y(1)} />
+            <text x={x(Date.now())} y={y(0)} dy={-2} text-anchor="left"></text>
+        </g>
+    </svg>
+</div>
+
+<style lang="scss">
+    select {
+        width: 100px;
+    }
+
+    div.timezone {
+        display: flex;
+        flex-direction: row nowrap;
+        align-items: stretch;
+
+        margin-bottom: 0.3em;
+
+        gap: 0.5em;
+        
+        & > div {
+            flex-basis: 0 0;
+
+            display: flex;
+            flex-direction: row nowrap;
+            align-items: stretch;
+            gap: 0.2em;
+        }
+
+        & > svg {
+            flex-grow: 1;
+        }
+    }
+
+    svg {
+        text {
+            font-size: 10px;
+            font-family: sans-serif;
+        }
+    }
+
+    g.ticks {
+        stroke: #999;
+    }
+
+    g.now {
+        --color-now: #f00;
+
+        line {
+            stroke: var(--color-now);
+        }
+
+        text {
+            fill: var(--color-now);
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,127 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { base } from "$app/paths";
+	import Timezone from "$lib/components/timezone.svelte";
+	import { csvParseRows } from "d3";
+	import { onMount } from "svelte";
+	import { derived, readable } from "svelte/store";
+
+
+	/**
+	 * zone_name,country_code,abbreviation,time_start,gmt_offset,dst
+	 * @typedef {Object} Timezone
+	 * @property {string} zone_name
+	 * @property {string} country_code
+	 * @property {string} abbreviation
+	 * @property {EpochTimeStamp} time_start 
+	 * @property {number} gmt_offset in seconds
+	 * @property {boolean} dst Whether daylight savings is in effect
+	 */
+
+	/**
+	 * Load the list of timezones from time_zone.csv
+	 * @returns {Promise<Record<string, Timezone[]>>}
+	 */
+	async function loadTimezones() {
+		const response = await fetch(`${base}/time_zone.csv`);
+		const text = await response.text();
+		const timezoneEntries = csvParseRows(text);
+
+		/** @type {Record<string, Timezone[]>} */
+		let _timezones = {};
+		for (let entry of timezoneEntries) {
+			let [zone_name, country_code, abbreviation, time_start, gmt_offset, dst] = entry;
+			if (!_timezones[zone_name]) {
+				_timezones[zone_name] = [];
+			}
+			_timezones[zone_name].push({
+				zone_name,
+				country_code,
+				abbreviation,
+				time_start: +time_start,
+				gmt_offset: +gmt_offset,
+				dst: dst === "1"
+			});
+		}
+
+		timezones = readable(_timezones);
+
+		return _timezones;
+	}
+
+
+
+	async function loadCountries() {
+		const response = await fetch(`${base}/country.csv`);
+		const text = await response.text();
+		const countryEntries = csvParseRows(text);
+
+		/** @type {Record<string, string>} */
+		let _countries = {};
+		for (let entry of countryEntries) {
+			let [country_code, country_name] = entry;
+			_countries[country_code] = country_name;
+		}
+		
+		countries = readable(_countries);
+
+		return _countries;
+
+	}
+
+	let countries = readable({});
+
+
+	/**
+	 * A readable store containing the list of timezones.
+	 * @type {import("svelte/store").Readable<Record<string, Timezone[]>>}
+	 */
+	let timezones = readable({});
+
+	onMount(()=>{
+		loadTimezones();
+		loadCountries();
+	});
+
+	
+
+	/**
+	 * A derived store containing searchable timezone names and their respective timezones.
+	 * @type {import("svelte/store").Readable<Record<string, string>>}
+	 */
+	$: timezoneNames = derived([timezones, countries], ([$timezones, $countries]) => {
+
+		let countryToTimezoneMap = Object.entries($countries).map(
+			([country_code, country_name])=>
+			[country_name, Object.values($timezones).map(tzs=>tzs[0]).find(tz=>tz.country_code == country_code)?.zone_name]
+		);
+
+		let timezoneNameEntries = [
+			...Object.keys($timezones).map(tz=>[tz, tz]), 
+			...countryToTimezoneMap
+		];
+
+		let timezoneNames = Object.fromEntries(timezoneNameEntries);
+		return timezoneNames;
+	});
+
+
+	const defaultTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+	let selectedTimezones = [defaultTimezone];
+
+	$: console.log(selectedTimezones);
+</script>
+<h1>Timezone Events</h1>
+A simple way to coordinate cross-timezone events.
+
+<p>
+	{#each selectedTimezones as timezone}
+		<Timezone {timezoneNames} {timezones} bind:value={timezone} />
+	{/each}
+	<button on:click={() => selectedTimezones = [...selectedTimezones, defaultTimezone]}>Add Timezone</button>
+</p>
+
+<p>
+Cities data from <a href="https://simplemaps.com/data/world-cities">https://simplemaps.com/data/world-cities</a>.<br/>
+Time zones from <a href="https://www.iana.org">https://www.iana.org</a> processed by <a href="https://timezonedb.com">https://timezonedb.com</a>
+</p>
